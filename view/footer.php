e47bfd7d5eee2042af7a21f1feddd690070bd28d
vongoctinh1002 <tinhvanvo1999@gmail.comgit config --global user.email tinhvanvo1999@gmail.comgit config --global user.email tinhvanvo1999@gmail.com>
2021-05-13 12:00:00 +0700

    done

---
+++ view/footer.php
@@ -0,0 +1,25 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-4 wow zoomIn">
+				<img src="images/logo.jpg">
+				<p>Chất lượng - Đảm bảo - Phục vụ</p>
+				<p>Chính sách đổi trả</p>
+				<p>Giao hàng và thanh toán</p>
+			</div>
+			<div class="col-xs-12 col-sm-4 wow zoomIn mxh">
+				<h3>Follow</h3>
+				<i class="fa fa-facebook"></i><span>Facebook.com</span><br>
+				<i class="fa fa-instagram"></i><span>Instagram.com</span><br>
+				<i class="fa fa-twitter"></i><span>Twitter.com</span><br>
+			</div>
+			<div class="col-xs-12 col-sm-4 wow zoomIn mxh">
+				<h3>Liên hệ</h3>
+				<i class="fa fa-map-marker"></i><span>96 Nguyen Hue, TP. Hue</span><br>
+				<i class="fa fa-phone"></i><span>01655205223</span>
+			</div>
+			
+		</div>
+	</div>
+	<button onclick="topFunction()" id="backtotop" title="Go to top" class="btn"><i class="fa fa-arrow-up"></i></button>
+</footer>
